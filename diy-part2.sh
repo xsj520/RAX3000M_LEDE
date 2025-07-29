a04d20e4c56a23bdec9b7c037dbbb8073c91eb09
xsj520 <72717602+xsj520@users.noreply.github.com>
2025-07-29 15:22:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
