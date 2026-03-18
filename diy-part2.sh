5caa9cb829ea245b380903fcacb24eb1de416a55
xsj520 <72717602+xsj520@users.noreply.github.com>
2026-03-18 15:18:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,9 +32,9 @@ else
 fi
 
 # 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
+# date_version=$(date +"%y.%m.%d")
 # orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by xsj/g" package/lean/default-settings/files/zzz-default-settings
+# sed -i "s/${orig_version}/R${date_version} by xsj/g" package/lean/default-settings/files/zzz-default-settings
 
 # git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser
 # 修改插件名字
