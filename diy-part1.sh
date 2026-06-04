6889c2dd580ce8db7f66e23306204f67cd42c892
xsj520 <72717602+xsj520@users.noreply.github.com>
2026-06-04 08:52:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,24 +8,13 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# ==========================
-# 1️⃣ 添加官方 feed
-# ==========================
-# DDNSTO 官方 feed
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 
 # 其他常用 feed
 echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
 echo 'src-git mt5700webui https://github.com/aliass532/mt5700webui-openwrt-server.git;main' >> feeds.conf.default
 
-# ==========================
-# 2️⃣ 安装 feed
-# ==========================
-./scripts/feeds update -a
-./scripts/feeds install -a
 
-# ==========================
 # 3️⃣ 添加第三方 feed (可选)
-# ==========================
+
 # git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 
