cd52cfd1dbc099df7b6e015692ade83943ca0a48
xsj520 <72717602+xsj520@users.noreply.github.com>
2026-08-23 22:03:30 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -8,6 +8,8 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
+# 禁用node‑host，规避Python2.7依赖
+sed -i 's/CONFIG_PACKAGE_node-host=y/# CONFIG_PACKAGE_node-host is not set/g' .config
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
